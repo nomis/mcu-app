1eb70f39ddfea4a8c9df4ae2e4285835fd660931
Simon Arlott <sa.me.uk>
2022-11-27 16:31:06 +0000

    Find the size of the whole TLS area for esp32 binaries

--- pio/app-tls-size.py
+++ pio/app-tls-size.py
@@ -26,10 +26,24 @@ import re
 import subprocess
 import sys
 
+RE_ELF_SECTION = re.compile(r"^\s*(?P<type>\w+)\s+(?P<offset>\w+)\s+(?P<virtaddr>\w+)\s+(?P<physaddr>\w+)\s+(?P<filesiz>\w+)\s+(?P<memsiz>\w+)\s+(?P<ndx>\w+)\s+")
 Symbol = collections.namedtuple("Symbol", ["value", "size", "line"])
-RE_ELF_SYMBOL = re.compile(r"^\s*(?P<num>\w+):\s+(?P<value>\w+)\s+(?P<size>\w+)\s+(?P<type>\w+)\s+")
+RE_ELF_SYMBOL = re.compile(r"^(?P<before_value>\s*(?P<num>\w+):\s+)(?P<value>\w+)(?P<after_value>\s+(?P<size>\w+)\s+(?P<type>\w+)\s+(?P<bind>\w+)\s+(?P<visibility>\w+)\s+(?P<ndx>\w+)\s+(?P<name>\w+))")
 
 def print_tls_size(fw_elf):
+	tls_offset = None
+	width = 8
+
+	lines = subprocess.run(["readelf", "-W", "--program-headers", fw_elf],
+			check=True, universal_newlines=True, stdout=subprocess.PIPE
+		).stdout.strip().split("\n")
+
+	for line in lines:
+		match = RE_ELF_SECTION.match(line)
+		if match:
+			if tls_offset is None and match["type"] == "TLS":
+				tls_offset = int(match["virtaddr"], 16)
+
 	header = True
 	lines = subprocess.run(["readelf", "-W", "--syms", "--dyn-syms", fw_elf],
 			check=True, universal_newlines=True, stdout=subprocess.PIPE
@@ -43,6 +57,16 @@ def print_tls_size(fw_elf):
 
 			if match["type"] == "TLS":
 				syms.add(Symbol(int(match["value"], 16), int(match["size"]), line))
+				width = len(match['value'])
+			elif tls_offset is not None and (match["type"] == "NOTYPE" and match["bind"] == "GLOBAL"
+					and match["visibility"] == "DEFAULT"
+					and match["name"] in set(["_thread_local_start", "_thread_local_end"])
+			):
+				value = int(match["value"], 16) - tls_offset
+				line = ("{1}{2:0{0}x}{3}").format(len(match['value']),
+					match["before_value"], value, match["after_value"])
+				syms.add(Symbol(value, int(match["size"]), line))
+
 		elif header:
 			print(line)
 
@@ -53,8 +77,12 @@ def print_tls_size(fw_elf):
 	else:
 		size = 0
 
+	value = syms[0].value
 	for sym in syms:
+		if sym.value > value:
+			print("\t{1:0{0}x} {2:5d} TLS            UNKNOWN".format(width, value, sym.value - value))
 		print(sym.line)
+		value = sym.value + sym.size
 
 	print()
 	print(f"Total Thread-Local Storage size: {size} bytes")
